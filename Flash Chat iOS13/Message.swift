4fb221dffea96a49523b98578c76b4712d86510a
Taha Enes Aslantürk <enes.aslanturk@ug.bilkent.edu.tr>
2022-05-01 18:23:26 +0300

    upd

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,15 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Taha Enes Aslantürk on 1.05.2022.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+    
+}
